42448065647de0b42902d5f0dc77a2627a222bfd
aditya-2k23 <holaaditya123@gmail.com>
2025-06-09 18:31:03 +0530

    Add Starter files
    Initialize with the project and tweak the layout of the app.
    
    Removing all the styles from the globals.css

---
+++ app/dashboard/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Dashboard from '@/components/Dashboard';
+import Main from '@/components/Main';
+import Login from '@/components/Login';
+
+export const metadata = {
+  title: "Moody ⋅ Dashboard",
+};
+
+export default function DashboardPage() {
+  const isAuthenticated = false;
+  let children = (
+    <Login />
+  )
+
+  if (isAuthenticated) {
+    children = (
+      <Dashboard />
+    )
+  }
+
+  return (
+    <Main>{children}</Main>
+  )
+}
