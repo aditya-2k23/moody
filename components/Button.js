b91746cd9cb72caaeb8fd63a7dc02e0fee686ad8
aditya-2k23 <holaaditya123@gmail.com>
2025-06-18 11:35:06 +0530

    Fix Button component to allow custom className for normal state

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,10 @@
 export default function Button({ text, dark, full, onClick, normal = true, className = "" }) {
   return (
     normal ? (
-      <button onClick={onClick} className={`${dark ? `text-white bg-indigo-600` : `text-indigo-600`} border-2 border-solid border-indigo-600 rounded-full overflow-hidden transition duration-200 hover:opacity-60 ${full ? "grid place-items-center w-full" : ""}`}>
+      <button onClick={onClick} className={`${dark ? `text-white bg-indigo-600` : `text-indigo-600`} border-2 border-solid border-indigo-600 rounded-full overflow-hidden transition duration-200 hover:opacity-60 ${full ? "grid place-items-center w-full" : ""} ${className}`}>
         <p className="fugaz px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3">{text}</p>
-      </button>) : (
+      </button>
+    ) : (
       <button className={`${className}`} onClick={onClick}>
         <p className="fugaz whitespace-nowrap">{text}</p>
       </button>
