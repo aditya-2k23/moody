e52e84877bb66ca99cded82a94bde96731ebdf72
aditya-2k23 <holaaditya123@gmail.com>
2025-06-20 14:57:06 +0530

    Refines Logout component styling and enhances button functionality

--- components/Logout.js
+++ components/Logout.js
@@ -9,9 +9,9 @@ export default function Logout() {
   if (!currentUser) return null;
 
   return (
-    <div className="flex items-center justify-center gap-2">
-      <p className="text-sm sm:text-base md:text-lg font-sans text-slate-700 font-semibold fugaz">Hello <span className="textGradient">{currentUser.email.split("@")[0]}</span>👋</p>
-      <Button text={<>Log Out<i className="ml-1 sm:ml-2 fa-solid fa-right-from-bracket text-indigo-600"></i></>} onClick={logOut} />
+    <div className="flex items-center justify-center gap-2 md:gap-3 text-indigo-600 hover:text-white transition duration-200">
+      <p className="text-xs sm:text-base md:text-lg text-slate-700 font-semibold fugaz">Hello <span className="textGradient">{currentUser.email.split("@")[0]}</span>👋</p>
+      <Button text={<>Log Out<i className="ml-1 sm:ml-2 fa-solid fa-right-from-bracket"></i></>} onClick={logOut} normal={false} className="text-xs sm:text-base md:text-lg border-2 border-indigo-600 rounded-full text-slate-600 hover:text-white hover:bg-indigo-600 overflow-hidden transition duration-200 px-2 md:px-4 py-1" />
     </div>
   )
 }
