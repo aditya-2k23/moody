0efdf18eceb6968f1b6efc0862fcce2748b77ad9
aditya-2k23 <holaaditya123@gmail.com>
2025-07-03 16:03:43 +0530

    Enhances Loader component to accept dynamic size prop for improved flexibility

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Loader() {
+export default function Loader({ size = '5xl' }) {
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
-      <i className="fas fa-spinner animate-spin text-4xl text-indigo-600 sm:text-5xl"></i>
+      <i className={`fas fa-spinner animate-spin text-4xl text-indigo-600 sm:text-${size}`}></i>
     </div>
   )
 }
