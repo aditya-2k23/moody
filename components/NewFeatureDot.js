8b98ab4a1458bdeac65e821a5f49e7eacd5b1ae2
aditya-2k23 <holaaditya123@gmail.com>
2026-02-08 19:43:47 +0530

    Enhance NewFeatureDot to be positioned absolutely for feature highlights

--- components/NewFeatureDot.js
+++ components/NewFeatureDot.js
@@ -7,7 +7,7 @@
 export default function NewFeatureDot({ className = "" }) {
   return (
     <span
-      className={`w-2 h-2 bg-red-500 rounded-full ${className}`}
+      className={`absolute w-2 h-2 bg-red-500 rounded-full z-30 ${className}`}
       aria-label="New feature"
     />
   );
