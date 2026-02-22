6744e9116cccd3d16c4da2ea6599776f58151712
aditya-2k23 <holaaditya123@gmail.com>
2026-02-22 19:51:53 +0530

    Implements advanced dynamic glow background with GSAP

--- components/Main.js
+++ components/Main.js
@@ -1,10 +1,7 @@
-import GlowBackground from "./GlowBackground";
-
 export default function Main(props) {
   const { children } = props;
   return (
     <main className="flex-1 flex flex-col p-4 sm:p-8 relative overflow-x-clip">
-      <GlowBackground />
       {children}
     </main>
   );
