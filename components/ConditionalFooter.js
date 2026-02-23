1fa6711afe1b9d0cdd4fbc55deb19792b3bf56f5
aditya-2k23 <holaaditya123@gmail.com>
2026-02-24 01:48:41 +0530

    Refines footer layout and spacing for improved aesthetics

--- components/ConditionalFooter.js
+++ components/ConditionalFooter.js
@@ -12,11 +12,11 @@ export default function ConditionalFooter() {
   if (pathname === "/") return null;
 
   return (
-    <footer className="p-4 sm:p-8 pt-2 sm:pt-0 flex justify-between text-sm md:text-base">
+    <footer className="p-4 md:px-8 flex justify-between text-sm md:text-base">
       <p className="text-indigo-500 dark:text-indigo-400 fugaz cursor-default">
         Created with 💜 by Aditya
       </p>
-      <div className="flex gap-4">
+      <div className="flex gap-2 sm:gap-4">
         <Link
           href="https://www.github.com/aditya-2k23/moody"
           target="_blank"
