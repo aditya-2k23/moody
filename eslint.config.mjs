54bb9d9d5e07ddc94ecea767c7209b6ecd65b511
aditya-2k23 <holaaditya123@gmail.com>
2026-06-03 02:19:35 +0530

    update config for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextConfig from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = [
   {
     ignores: [".next/**", "node_modules/**", "dist/**", "coverage/**"]
   },
-  ...compat.extends("next/core-web-vitals"),
+  ...nextConfig,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/static-components": "off",
+      "react-hooks/immutability": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
